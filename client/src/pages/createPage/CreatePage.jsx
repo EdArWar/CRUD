90847a85d59a4cdfc94309a6b6505f66d6535ffa
EdArWar <edgar.vard92@gmail.com>
2021-12-31 14:18:26 +0400

    create create page

--- client/src/pages/createPage/CreatePage.jsx
+++ client/src/pages/createPage/CreatePage.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import FileBase from "react-file-base64";
+import "./CreatePage.css";
 
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [userName, setUserName] = useState("");
+  const [userProfession, setUserProfession] = useState("");
+  const [userPosition, setUserPosition] = useState("");
+  const [userPicture, setUserPicture] = useState("");
+
+  const onCreateHandle = () => {
+    console.log("userName", userName);
+    console.log("userProfession", userProfession);
+    console.log("userPosition", userPosition);
+    console.log("userPicture", userPicture);
+  };
+
+  return (
+    <Container className="create_page_container">
+      <Row>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>User Profession</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Profession"
+            value={userProfession}
+            onChange={(e) => setUserProfession(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>User Position</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Position"
+            value={userPosition}
+            onChange={(e) => setUserPosition(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>User Picture</Form.Label>
+          {/* <Form.Control type="file" /> */}
+          <Row>
+            <FileBase
+              type="file"
+              multiple={false}
+              onDone={({ base64 }) => {
+                console.log("base64", base64);
+                setUserPicture(base64);
+              }}
+            />
+          </Row>
+        </Form.Group>
+        <Button variant="primary" onClick={onCreateHandle}>
+          Submit
+        </Button>
+      </Row>
+    </Container>
+  );
 };
 
 export default CreatePage;
