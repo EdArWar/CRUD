3df0e3fac17e59d4905a1ac26ec4ff571ff21f98
EdArWar <edgar.vard92@gmail.com>
2022-01-01 14:04:05 +0400

    create login and  registration

--- client/src/components/signIn/login/Login.jsx
+++ client/src/components/signIn/login/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    return () => {
+      setEmail("");
+      setPassword("");
+    };
+  }, []);
+
+  return (
+    <Container className="login_container">
+      <Row className="justify-content-md-center">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary">Submit</Button>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
--- client/src/components/signIn/registration/Registration.jsx
+++ client/src/components/signIn/registration/Registration.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import "./Registration.css";
 
 const Registration = () => {
-  return <div>Registration</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    return () => {
+      setEmail("");
+      setName("");
+      setPassword("");
+    };
+  }, []);
+
+  return (
+    <Container className="registration_container">
+      <Row className="justify-content-md-center">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary">Submit</Button>
+      </Row>
+    </Container>
+  );
 };
 
 export default Registration;
