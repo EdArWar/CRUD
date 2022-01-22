1bf29540b36fe6f50a3c773a9cd28eb41a69dded
EdArWar <edgar.vard92@gmail.com>
2022-01-22 15:56:09 +0400

    minor fix

--- client/src/utils/Utils.js
+++ client/src/utils/Utils.js
@@ -1,11 +1,3 @@
-export function wordToUpperCase(str) {
-  return str;
-}
-
 export const isNotEmpty = (obj) => {
   return !!obj && Object.keys(obj).length !== 0;
-};
-
-export const isEmpty = (obj) => {
-  return !obj || !Object.keys(obj).length;
 };
